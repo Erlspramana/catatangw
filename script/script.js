538c1c7dff09db6bb4ee28ca43474ada4c79dea0
erlspramana <erlandpramana1@gmail.com>
2026-01-29 18:58:19 +0700

    a

--- script/script.js
+++ script/script.js
@@ -49,29 +49,6 @@ function moveCursor2(e) {
 }
 
 //
-
-document.querySelectorAll('button').forEach(a =>
-  a.addEventListener('mousemove', function() {
-  cursor.classList.add('hover-cursor');
-    }));
-
-document.querySelectorAll('button').forEach(a =>
-  a.addEventListener('mouseleave', function() {
-    cursor.classList.remove('hover-cursor');
-      }));
-
-//
-
-document.querySelectorAll('a').forEach(a =>
-  a.addEventListener('mousemove', function() {
-  cursor.classList.add('hover-cursor-a');
-    }));
-
-document.querySelectorAll('a').forEach(a =>
-  a.addEventListener('mouseleave', function() {
-    cursor.classList.remove('hover-cursor-a');
-      }));
-
 //
 
 // For Bookmark Links
